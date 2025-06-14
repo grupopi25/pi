a9920ae44712e72b0a044836bd6688daf8d01156
JoseCandeia <josecandeia021a2@gmail.com>
2025-06-14 11:47:20 -0300

    adicionandos as telas para mensagens

---
+++ app/Livewire/MessageLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MessageLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.message-livewire');
+    }
+}
